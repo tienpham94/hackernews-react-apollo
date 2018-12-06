b3d354d700a3a3dd6caf14d25f0df0a470614f2d
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-06 11:55:00 +0200

    Update code style

--- src/components/Link.js
+++ src/components/Link.js
@@ -33,20 +33,20 @@ class Link extends Component {
       <span className="gray">{this.props.index + 1}.</span>
       {authToken && (
         <Mutation
-  mutation={VOTE_MUTATION}
-  variables={{ linkId: this.props.link.id }}
-  update={(store, { data: { vote } }) =>
-    this.props.updateStoreAfterVote(store, vote, this.props.link.id)
-  }
->
-  {voteMutation => (
-    <div className="ml1 gray f11" onClick={voteMutation}>
-      ▲
-    </div>
-  )}
-</Mutation>
+          mutation={VOTE_MUTATION}
+          variables={{ linkId: this.props.link.id }}
+          update={(store, { data: { vote } }) =>
+            this.props.updateStoreAfterVote(store, vote, this.props.link.id)
+          }
+        >
+          {voteMutation => (
+            <div className="ml1 gray f11" onClick={voteMutation}>
+              ▲
+            </div>
+          )}
+        </Mutation>
         )}
-      </div>
+        </div>
         <div className="ml1">
           <div>
             {this.props.link.description} ({this.props.link.url})
