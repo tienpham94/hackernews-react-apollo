4c8682176f12f4ffba51cfe07340a416c3a7495a
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-06 11:44:24 +0200

    Filtering

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,19 @@ import Header from './Header'
 import CreateLink from './CreateLink'
 import LinkList from './LinkList'
 import Login from './Login'
+import Search from './Search'
 
 class App extends Component {
   render() {
     return (
-      <div className="center w85">
+      <div className='center w85'>
         <Header />
-        <div className="ph3 pv1 background-gray">
+        <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path="/" component={LinkList} />
-            <Route exact path="/create" component={CreateLink} />
-            <Route exact path="/login" component={Login} />
+            <Route exact path='/' component={LinkList} />
+            <Route exact path='/create' component={CreateLink} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/search' component={Search} />
           </Switch>
         </div>
       </div>
